867afd7af9b4f3371c6af8d62ab87a1636a62702
yadavpr <pankneo@ccs.neu.edu>
2014-09-28 17:54:33 -0400

    Adding merger project

---
+++ PdfMerger/Merger.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Merger.aspx.cs" Inherits="Merger" %>
+<%@ Register Assembly="AjaxControlToolkit"  Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>PDF MERGER TOOL</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="CSS/merge.css" type="text/css" media="screen"/>
+    <script type="text/javascript">
+
+       
+</script>
+    <script>
+
+    </script>
+
+ 
+</head>
+
+<body>
+    <form id="form1" runat="server" >
+        <div class="container">
+            <div class="row vertical-center-row">
+                <div class="col-lg-12">
+                    <div class="row ">
+                        <div class="col-md-8 col-md-offset-2 col-sm-6 col-sm-offset-2 col-xs-6 col-xs-offset-2">
+                            <div>
+
+                                <asp:Panel ID="Panel1" runat="server">
+                                    <table class="tab">
+                                        <tr>
+                                            <td style="text-align: center">
+                                                <label>
+                                                    <h1>PDF MERGER</h1>
+                                                </label>
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>
+                                                <ajaxToolkit:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server" />
+                                                <ajaxToolkit:AjaxFileUpload ID="AjaxFileUpload1" OnClientUploadComplete="" runat="server" ThrobberID="myThrobber"  MaximumNumberOfFiles="10" AllowedFileTypes="" OnUploadComplete="AjaxFileUpload1_UploadComplete1" />
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <asp:Button Text="Start" ID="btnStart" CssClass="btn-block btn-primary btn-lg" runat="server" OnClick="btnStart_Click" /></td>
+                                        </tr>
+                                    </table>
+
+
+                                </asp:Panel>
+
+                            </div>
+                </div>
+            </div>
+                </div>
+            </div>
+        </div>
+
+
+    </form>
+</body>
+</html>
---
+++ PdfMerger/Merger.aspx.cs
@@ -0,0 +1,156 @@
+﻿using AjaxControlToolkit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.Text;
+
+public partial class Merger : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+   
+
+    protected void AjaxFileUpload1_UploadCompleteAll1(object sender, AjaxFileUploadCompleteAllEventArgs e)
+    {
+
+
+        string path = Server.MapPath("~/PDFs/");
+        string[] filenames = System.IO.Directory.GetFiles(path);
+        DirectoryInfo dir = new DirectoryInfo(path);
+
+        int files = dir.GetFiles().Count();
+        if (files > 0)
+        {
+
+
+            //PdfReader pdfAddenda = new PdfReader(DOcUload.PostedFile.InputStream);
+            //MemoryStream streamAddenda = new MemoryStream();
+            //PdfStamper stamperAddenda = new PdfStamper(pdfAddenda, streamAddenda);
+            //stamperAddenda.Close();
+            //pdfAddenda.Close();
+            //streamAddenda.Flush();
+            //streamAddenda.Close();
+            //byte[] addenda = streamAddenda.ToArray();
+
+
+            //List<byte[]> pdfs = new List<byte[]>();
+            //pdfs.Add(contract);
+            //pdfs.Add(addenda);
+            //String result = Path.GetTempPath() + "output.pdf";
+
+            //Document document = new Document();
+            ////create PdfCopy object
+            //PdfCopy copy = new PdfCopy(document, new FileStream(result, FileMode.Create));
+            ////open the document
+            //document.Open();
+            ////PdfReader variable
+            //PdfReader reader;
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    //create PdfReader object
+            //    reader = new PdfReader(pdfs[i]);
+            //    //merge combine pages
+            //    for (int page = 1; page <= reader.NumberOfPages; page++)
+            //        copy.AddPage(copy.GetImportedPage(reader, page));
+
+            //}
+            ////close the document object
+            //document.Close();
+
+           
+        }
+        else
+        {
+            Response.Write("<script>alert('Please add atleast two Documents to merge.');</script>");
+
+        }
+    }
+    protected void btnStart_Click(object sender, EventArgs e)
+    {
+        string path = Server.MapPath("~/PDFs/");
+        string[] filenames = System.IO.Directory.GetFiles(path);
+        DirectoryInfo dir = new DirectoryInfo(path);
+
+        int files = dir.GetFiles().Count();
+        if (files > 0)
+        {
+
+            String result = Server.MapPath("~/Merged/Output.pdf");
+            //create Document object
+            Document document = new Document();
+            //create PdfCopy object
+            PdfCopy copy = new PdfCopy(document, new FileStream(result, FileMode.Create));
+            //open the document
+            document.Open();
+            //PdfReader variable
+            PdfReader reader;
+            for (int i = 0; i < filenames.Length; i++)
+            {
+                //create PdfReader object
+                reader = new PdfReader(filenames[i]);
+                //merge combine pages
+                for (int page = 1; page <= reader.NumberOfPages; page++)
+                    copy.AddPage(copy.GetImportedPage(reader, page));
+                reader.Close();
+
+
+            }
+            //close the document object
+            document.Close();
+            copy.Close();
+            //File.Open(Server.MapPath("~/Merged/Output.pdf"), FileMode.Open);
+            foreach (FileInfo fi in dir.GetFiles())
+            {
+                fi.Delete();
+            }
+
+            FileOpen();
+        }
+
+    }
+
+    protected void FileOpen()
+    {
+        string path = Server.MapPath("~/Merged/Output.pdf"); //get file object as FileInfo  
+        FileInfo file = new FileInfo(path); //-- if the file exists on the server  
+
+
+        if (file.Exists) //set appropriate headers  
+        {
+            Response.Clear();
+            Response.AddHeader("Content-Disposition", "inline; filename=" + file.Name);
+            Response.AddHeader("Content-Length", file.Length.ToString());
+            Response.ContentType = "application/octet-stream";
+
+
+            // write file to browser
+            Response.WriteFile(file.FullName);
+
+
+            Response.End();
+        }
+        else
+        {
+            // if file does not exist
+            Response.Write("This file does not exist.");
+        }
+    }
+
+    protected void AjaxFileUpload1_UploadComplete1(object sender, AjaxFileUploadEventArgs e)
+    {
+        string filePath = "~/PDFS/" + e.FileName;
+
+        AjaxFileUpload1.SaveAs(MapPath(filePath));
+    }
+}
